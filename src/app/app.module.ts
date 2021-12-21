aa575aaf99f1852fef9a2f3c2c6362d8e456bc34
vu <vik.ugrin@gmail.com>
2021-12-21 10:10:36 +0100

    feat: add fire and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'track-it-store',
+    appId: '1:730627651330:web:154e5104e0848e0762741b',
+    storageBucket: 'track-it-store.appspot.com',
+    apiKey: 'AIzaSyAdu8WGUjgMIp3Bvgrlqma31R7zxmcTKHM',
+    authDomain: 'track-it-store.firebaseapp.com',
+    messagingSenderId: '730627651330',
+  },
   production: true
 };
